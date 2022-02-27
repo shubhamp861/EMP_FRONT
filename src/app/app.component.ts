c67d503385aa8aa6a7f7b5328c4054d9e6cfaa1f
shubhamp861 <46345008+shubhamp861@users.noreply.github.com>
2022-02-27 15:43:44 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { Student } from './student';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'emp';
