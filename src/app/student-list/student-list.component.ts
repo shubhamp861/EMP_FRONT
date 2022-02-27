5b1288d6699cebfde2dd200a5e13c9026b6b421d
shubhamp861 <46345008+shubhamp861@users.noreply.github.com>
2022-02-27 15:38:45 +0530

    Update student-list.component.ts

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -7,8 +7,7 @@ import {FormControl,FormGroup,Validators} from '@angular/forms';
   
 @Component({  
   selector: 'app-student-list',  
-  templateUrl: './student-list.component.html',  
-  styleUrls: ['./student-list.component.css']  
+  templateUrl: './student-list.component.html'
 })  
 export class StudentListComponent implements OnInit {  
   
